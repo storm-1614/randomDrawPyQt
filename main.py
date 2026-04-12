854c486ecd9da2c65f92437b2f2da213d53b7089
storm-1614 <storm1614@outlook.com>
2026-04-12 09:33:10 +0800

    label and button

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
 
 app = QApplication(sys.argv)
+
+# 创建窗口
 window = QWidget()
 window.setWindowTitle("random draw app")
+
+layout = QVBoxLayout()
+
+label = QLabel("请抽签")
+layout.addWidget(label)
+
+button = QPushButton("抽取")
+layout.addWidget(button)
+
+window.setLayout(layout)
 window.show()
 sys.exit(app.exec_())

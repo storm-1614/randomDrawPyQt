f7c73dfadc734ecfb536b03db0b6b6944b6c4417
storm-1614 <storm1614@outlook.com>
2026-04-12 10:42:14 +0800

    modify: oop

--- main.py
+++ main.py
@@ -4,67 +4,64 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
 from PyQt5.QtCore import Qt
 
-# 测试用的列表
-name_test_list = ["A", "B", "C", "D"]
-
-def read_file(path)->list:
-    with open(path, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-    return lines
-
-name_list = read_file("./list/test")
-
 app = QApplication(sys.argv)
 
-# 创建窗口
-window = QWidget()
-window.setWindowTitle("random draw app")
-window.setGeometry(100, 100, 400, 300)
 
-# 创建布局
-layout = QVBoxLayout()
+class RandomDrawApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("random draw app")
+        self.setGeometry(100, 100, 400, 300)
+        self.names = self.load_names("./list/list")
+        # 初始化布局
+        self.base_layout = QVBoxLayout()
+        # 标签（显示结果）
+        self.result_label = QLabel("请抽签")
+        # 按钮， 开始随机
+        self.random_button = QPushButton("抽取")
 
-# 标签（显示结果）
-label = QLabel("请抽签")
+    def load_names(self, path: str) -> list:
+        with open(path, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+        return lines
 
-# 字体
-font = QFont()
-font.setPointSize(48)
-font.setBold(True)
-label.setFont(font)
+    def init_UI(self):
+        # 标签样式
+        label_font = QFont()
+        label_font.setPointSize(48)
+        label_font.setBold(True)
+        self.result_label.setFont(label_font)
 
-# 设置居中对齐
-label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.result_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-# 设置字体样式：CSS
-label.setStyleSheet("""
-QLabel {
-    color: #0000FF
-}
-""")
+        self.result_label.setStyleSheet("""
+        QLabel {
+            color: #0000FF
+        }
+        """)  # CSS 美化
 
-layout.addWidget(label)
+        self.base_layout.addWidget(self.result_label)
 
-button = QPushButton("抽取")
+        self.random_button.setStyleSheet("""
+            QPushButton {
+                font-size = 20px;
+                padding: 15px;
+            }
+        """)
 
-button.setStyleSheet("""
-    QPushButton {
-        font-size: 20px;
-        padding: 15px;
-    }
-""")
+        self.random_button.clicked.connect(self.button_clicked)
+        self.base_layout.addWidget(self.random_button)
+        self.setLayout(self.base_layout)
+
+    def draw(self):
+        self.show()
+
+    def button_clicked(self):
+        select_name = random.choice(self.names)
+        self.result_label.setText(select_name)
 
 
-# 按钮点击事件
-def button_clicked():
-    select_name = random.choice(name_list)
-    label.setText(select_name)
-
-# 连接 clicked 信号到 button_clicked 函数
-button.clicked.connect(button_clicked)
-
-layout.addWidget(button)
-
-window.setLayout(layout)
-window.show()
+random_draw = RandomDrawApp()
+random_draw.init_UI()
+random_draw.draw()
 sys.exit(app.exec_())
